9a49dc29af51c99bbddcbfaecc3b9368d4b8ff45
mousedrool <richard.abrahamson@yahoo.com>
2021-09-15 13:49:22 -0500

    swap out to repository approach to get published projects list.

--- src/main/java/com/rca/photoshare/api/controller/ProjectsApiDelegateImpl.java
+++ src/main/java/com/rca/photoshare/api/controller/ProjectsApiDelegateImpl.java
@@ -1,27 +1,24 @@
 package com.rca.photoshare.api.controller;
 import com.rca.photoshare.api.ProjectsApiDelegate;
-import com.rca.photoshare.api.authorization.TokenModel;
 import com.rca.photoshare.api.model.Project;
+import com.rca.photoshare.api.database.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 @Service
 public class ProjectsApiDelegateImpl implements ProjectsApiDelegate {
-    @Override
-    public ResponseEntity<List<Project>> getProjects() {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("From /Projects: " + ((TokenModel)authentication.getPrincipal()).getSubject());
-        List<Project> projectsList = new ArrayList<Project>();
-        projectsList.add(new Project());
-        projectsList.add(new Project());
-        projectsList.add(new Project());
+    @Autowired
+    ProjectRepository projectsRepository;
+
+    @Override
+    public ResponseEntity<List<com.rca.photoshare.api.model.Project>> getProjects() {
+        List<Project> projectsList = projectsRepository.findByPublished(true);
         return ResponseEntity.ok(projectsList);
     }
+
+
 }
