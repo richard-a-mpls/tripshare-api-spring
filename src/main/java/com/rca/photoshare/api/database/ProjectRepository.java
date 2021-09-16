d8c16018913947e37efe54e5bf8b5572952d7618
mousedrool <richard.abrahamson@yahoo.com>
2021-09-16 13:21:33 -0500

    revert to boolean per the corrected definition

--- src/main/java/com/rca/photoshare/api/database/ProjectRepository.java
+++ src/main/java/com/rca/photoshare/api/database/ProjectRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface ProjectRepository extends MongoRepository<Project, String> {
     List<Project> findByProfileId(String profileId);
     // TODO, should be able to use boolean but string is currently stored in mongo
-    List<Project> findByPublished(String published);
+    List<Project> findByPublished(boolean published);
 }
