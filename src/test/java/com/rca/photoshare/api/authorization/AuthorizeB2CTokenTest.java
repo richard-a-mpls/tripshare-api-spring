623f82a48d3cd6e58c7df446f013126d4ecae776
mousedrool <richard.abrahamson@yahoo.com>
2021-09-26 14:37:16 -0500

    switch to junit instead of jupiter for assertequals.

--- src/test/java/com/rca/photoshare/api/authorization/AuthorizeB2CTokenTest.java
+++ src/test/java/com/rca/photoshare/api/authorization/AuthorizeB2CTokenTest.java
@@ -8,8 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
