599042a55246614bf09d05b8b4314bf16188f921
mousedrool <richard.abrahamson@yahoo.com>
2022-11-20 15:34:41 -0600

    feat: add env print

--- src/main/java/com/rca/photoshare/api/controller/WakeupApiDelegateImpl.java
+++ src/main/java/com/rca/photoshare/api/controller/WakeupApiDelegateImpl.java
@@ -1,15 +1,25 @@
 package com.rca.photoshare.api.controller;
 
 import com.rca.photoshare.api.WakeupApiDelegate;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service
 public class WakeupApiDelegateImpl implements WakeupApiDelegate {
 
+    final Log logger = LogFactory.getLog(this.getClass());
+
     @Override
     public ResponseEntity<Void> wakeup() {
+        Map envMap = System.getenv();
+        logger.info(envMap);
         return ResponseEntity.ok(null);
     }
 
